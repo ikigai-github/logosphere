237d6af761e86e486f32e57e0d576e22ec7a38f7
Dmitri Safine <dmitri.safine@adamantas.io>
2021-12-25 20:07:26 -0800

    Primer modules schemas

--- logosphere-primer/src/app.module.ts
+++ logosphere-primer/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { CatsModule } from './cats/cats.module'
+import { UserModule } from './user/user.module';
+import { MintingModule } from './minting/minting.module';
+import { AuctionModule } from './auction/auction.module';
 
 @Module({
   imports: [
@@ -9,6 +12,9 @@ import { CatsModule } from './cats/cats.module'
       typePaths: ['./**/*.graphql'],
       installSubscriptionHandlers: true,
     }),
+    UserModule,
+    MintingModule,
+    AuctionModule,
   ],
   providers: [],
 })
