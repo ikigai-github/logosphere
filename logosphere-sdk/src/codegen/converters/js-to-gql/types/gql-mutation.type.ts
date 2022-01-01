13ba190a2427368a9368af004bf24e813115772e
Dmitri Safine <dmitri.safine@adamantas.io>
2022-01-01 08:31:04 -0800

    Deleting js-to-gql directory

--- logosphere-sdk/src/codegen/converters/js-to-gql/types/gql-mutation.type.ts
+++
@@ -1,7 +0,0 @@
-import { GQLQueryType } from "./gql-query.type";
-
-/**
- * Tepresents a GQL mutation definition
- * note: I haven't seen a difference in the schema structure, so just copying the query for now.
- */
-export class GQLMutationType extends GQLQueryType {}
