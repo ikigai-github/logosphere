9c2e3d4b72336397f0135d8cf9bb0882ccd67a12
Dmitri Safine <dmitri.safine@adamantas.io>
2022-01-06 22:54:57 -0800

    JSON Schema Federated parser

--- logosphere-sdk/lib/codegen/json-schema/json-schema-federated.parser.spec.ts
+++ logosphere-sdk/lib/codegen/json-schema/json-schema-federated.parser.spec.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import { JsonSchemaFederatedParser } from './json-schema-federated.parser';
 import { ConfigurationLoader } from '../../configuration';
 import { FileSystemReader } from '../../readers';
+import { doesNotMatch } from 'assert';
 
 describe('Test JSON Schema Federated Parser', () => {
 
@@ -9,10 +10,10 @@ describe('Test JSON Schema Federated Parser', () => {
 
     const parser = new JsonSchemaFederatedParser();
     const configLoader = new ConfigurationLoader(new FileSystemReader(__dirname));
-    const config = await configLoader.load('../../configuration/configuration.fixture.json');
-    console.log(config);
-    const canonicalSchema = parser.parse(config);
+    const config = await configLoader.load('logosphere.json');
+    const canonicalSchema = await parser.parse(config);
     expect(canonicalSchema).toBeDefined();
+    console.log(JSON.stringify(canonicalSchema));
 
   });
 
--- logosphere-sdk/lib/codegen/json-schema/json-schema-federated.parser.ts
+++ logosphere-sdk/lib/codegen/json-schema/json-schema-federated.parser.ts
@@ -2,17 +2,23 @@ import { DefinitionType } from '..';
 import { IDefinition } from '../canonical.schema';
 import { Parser } from '../parser.abstract';
 import { Configuration, ModuleConfiguration } from '../../configuration';
-import { } from '../../readers';
+import { FileSystemReader } from '../../readers';
+import { JsonSchemaParser } from './json-schema.parser';
 
 
 export class JsonSchemaFederatedParser extends Parser {
   protected async getDefs(config: Configuration): Promise<IDefinition[]> {
-    config.modules.forEach((module: ModuleConfiguration) => {
-      
-
-
-    });
-    return [{name: '', type: DefinitionType.Enum, props: []}]
+    const reader = new FileSystemReader(__dirname);
+    const parser = new JsonSchemaParser();
+    const defs: IDefinition[] = [];
+    await Promise.all(
+      config.modules.map(async (module: ModuleConfiguration) => {
+        const schema = JSON.parse(await reader.read(module.jsonSchemaFile));
+        const canonical = await parser.parse(schema);
+        defs.push(...canonical.definitions);
+      })
+    );
+    return defs;
   }
 
 }
