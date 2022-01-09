ee18beaf7b9349cc7611bd52120d5ffeea5a5c9c
Dmitri Safine <dmitri.safine@adamantas.io>
2022-01-09 15:40:21 -0800

    Simplifying schema generator

--- logosphere-schematics/src/lib/schema/schema.factory.ts
+++ logosphere-schematics/src/lib/schema/schema.factory.ts
@@ -1,20 +1,4 @@
 import * as fs from 'fs';
-import { join, Path, strings } from '@angular-devkit/core';
-import {
-  apply,
-  mergeWith,
-  move,
-  empty,
-  Rule,
-  Source,
-  template,
-  url,
-  callRule,
-} from '@angular-devkit/schematics';
-import { basename, parse } from 'path';
-import {
-  DEFAULT_SCHEMA_TYPE,
-} from '../defaults';
 import { SchemaOptions } from './schema.schema';
 import { ConverterFactory } from '@logosphere/sdk/dist/lib/codegen/converters';
 import { 
@@ -23,24 +7,8 @@ import {
 } from '@logosphere/sdk/dist/lib/configuration';
 import { SchemaType } from '@logosphere/sdk/dist/lib/codegen';
 
-
-
 export function main(options: SchemaOptions): Function {
-  options = transform(options);
-  generate(options);
-  return () => { process.exit(0) } ;
-}
 
-function transform(options: SchemaOptions): SchemaOptions {
-  const target: SchemaOptions = Object.assign({}, options);
-  target.schemaType = !!target.schemaType ? target.schemaType : DEFAULT_SCHEMA_TYPE;
-
-  return target;
-}
-
-
-function generate(options: SchemaOptions): void {
-  
   const converter = ConverterFactory.getConverter(SchemaType.JsonFederated, options.schemaType as SchemaType);
   const config: Configuration = loadConfiguration();
   const targetSchema = converter.convert(config.modules);
@@ -52,5 +20,6 @@ function generate(options: SchemaOptions): void {
     fs.writeFileSync(`${modulePath}/${module}.schema.gql`, targetSchema[module], {flag: 'w+'});
     console.log(`Generated ${module}/${module}.schema.gql file`);
   });
-  
+
+  return () => { process.exit(0) } ;
 }
