c7745752d142c18292e1db032d820838d67d8726
Dmitri Safine <dmitri.safine@adamantas.io>
2022-04-28 19:48:07 -0700

    Serve GraphQL API

--- packages/sdk/src/generators/init/lib/add-dependencies.ts
+++ packages/sdk/src/generators/init/lib/add-dependencies.ts
@@ -5,6 +5,8 @@ import {
   nestJsSchematicsVersion,
   nestJsVersion7,
   nestJsVersion8,
+  nestJsApolloVersion,
+  nestJsGraphQLVersion,
   nxVersion,
   reflectMetadataVersion,
   rxjsVersion6,
@@ -37,12 +39,21 @@ export function addDependencies(tree: Tree): GeneratorCallback {
   return addDependenciesToPackageJson(
     tree,
     {
+      '@babel/core': '^7.0.0',
+      '@babel/preset-env': '^7.0.0',
+      '@nestjs/apollo': nestJsApolloVersion,
       '@nestjs/common': NEST_VERSION,
       '@nestjs/core': NEST_VERSION,
+      '@nestjs/graphql': nestJsGraphQLVersion,
       '@nestjs/platform-express': NEST_VERSION,
+      'apollo-server-fastify': '^3.5.0',
+      'babel-loader': '^8.0.2',
+      'fastify': '3.17.0',
+      'graphql': '^16.0.0',
       'reflect-metadata': reflectMetadataVersion,
       rxjs: RXJS,
       tslib: '^2.0.0',
+      webpack: '^2'
     },
     {
       '@nestjs/schematics': nestJsSchematicsVersion,
--- packages/sdk/src/utils/versions.ts
+++ packages/sdk/src/utils/versions.ts
@@ -3,6 +3,9 @@ export const nxVersion = '*';
 export const nestJsVersion7 = '^7.0.0';
 export const nestJsVersion8 = '^8.0.0';
 
+export const nestJsApolloVersion = '^10.0.0'
+export const nestJsGraphQLVersion = '^10.0.0'
+
 export const nestJsSchematicsVersion = '^8.0.0';
 
 export const rxjsVersion6 = '~6.6.3';
