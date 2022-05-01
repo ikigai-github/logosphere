c6128fa74d48f79dae0cfc6bbeeee338a24a11f0
Dmitri Safine <80562112+dmitri-safine@users.noreply.github.com>
2022-05-01 16:05:03 -0700

    Nx plugin (#23)
    * Workspace link script
    
    * Modifying app generator name prompt
    
    * Changing module generator name prompt

---
+++ link-workspace.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+echo ""
+echo "<<< Installing Logosphere SDK dependencies >>>"
+pnpm install
+
+echo ""
+echo "<<< Building Logosphere SDK libraries >>>"
+pnpm nx affected:build
+
+workspace=$1
+pushd ../${workspace}
+
+echo ""
+echo "<<< Linking workspace ${workspace} >>>"
+
+pnpm link ../logosphere/dist/libs/configuration
+pnpm link ../logosphere/dist/libs/converters
+pnpm link ../logosphere/dist/libs/decorators
+pnpm link ../logosphere/dist/libs/errors
+pnpm link ../logosphere/dist/libs/model
+pnpm link ../logosphere/dist/libs/readers
+pnpm link ../logosphere/dist/libs/sdk
+pnpm link ../logosphere/dist/libs/services/fluree
+pnpm link ../logosphere/dist/libs/utils
+popd
+echo ""
+echo "<<< Workspace ${workspace} has been linked >>>"
