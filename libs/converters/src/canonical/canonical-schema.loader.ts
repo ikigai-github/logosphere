5f0386efa8714e8ec4ab0b8479347c0e32a86eb0
Clifford Garvis <cgarvis@ikigaitech.org>
2022-05-03 11:27:46 -0700

    Scan for .entity files instead of .model

--- libs/converters/src/canonical/canonical-schema.loader.ts
+++ libs/converters/src/canonical/canonical-schema.loader.ts
@@ -2,28 +2,27 @@ import * as fs from 'fs-extra';
 import { CanonicalSchema } from './canonical.schema';
 import { getMetadataStorage } from '@logosphere/decorators';
 
+const getAllFiles = function (dirPath: string, arrayOfFiles?: string[]) {
+  const files = fs.readdirSync(dirPath);
 
-const getAllFiles = function(dirPath: string, arrayOfFiles?: string[]) {
-  const files = fs.readdirSync(dirPath)
+  arrayOfFiles = arrayOfFiles || [];
 
-  arrayOfFiles = arrayOfFiles || []
-
-  files.forEach(function(file) {
-    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+  files.forEach(function (file) {
+    if (fs.statSync(dirPath + '/' + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles);
     } else {
-      arrayOfFiles?.push(`${dirPath}/${file}`)
+      arrayOfFiles?.push(`${dirPath}/${file}`);
     }
-  })
+  });
 
-  return arrayOfFiles
-}
+  return arrayOfFiles;
+};
 
-export const canonicalSchemaLoader = (
-  sourceDir?: string
-): CanonicalSchema => {
+export const canonicalSchemaLoader = (sourceDir?: string): CanonicalSchema => {
   const dir = sourceDir ? sourceDir : `${process.cwd()}/dist`;
-  const entityFiles = getAllFiles(dir).filter((file) => file.endsWith('model.js'));
+  const entityFiles = getAllFiles(dir).filter((file) =>
+    file.endsWith('entity.js')
+  );
   entityFiles.map((file: string) => {
     eval(fs.readFileSync(file, 'utf-8'));
   });
