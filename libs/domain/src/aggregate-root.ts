7cd940c415143aa5a70f09d313b695703c742867
Dmitri Safine <dmitri.safine@adamantas.io>
2022-05-13 09:42:14 -0700

    LOG-140: Aggregate Root

--- libs/domain/src/aggregate-root.ts
+++ libs/domain/src/aggregate-root.ts
@@ -4,8 +4,4 @@ import { SHA256Identifier } from './sha256-identifier';
 export abstract class AggregateRoot<T> extends Entity<T> {
   // TODO: Add events support for aggregate roots using event system in Nest
   // https://ikigai-technologies.atlassian.net/browse/LOG-71
-
-  get id(): SHA256Identifier {
-    return this._identifier;
-  }
 }
