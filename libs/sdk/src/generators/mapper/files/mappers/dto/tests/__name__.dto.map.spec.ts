83fa27e1f3723d3c4730c95e7b82a8ca62cb0113
Dmitri Safine <dmitri.safine@adamantas.io>
2022-05-22 14:12:56 -0700

    LOG-154: DTO mapper templates

---
+++ libs/sdk/src/generators/mapper/files/mappers/dto/tests/__name__.dto.map.spec.ts
@@ -0,0 +1,36 @@
+import { <%= classify(name)%> } from '../../../entities/<%= dasherize(name) %>.entity';
+import { <%= classify(name)%><%= classify(type) %>Map } from '../<%= dasherize(name) %>.<%= dasherize(type) %>.map';
+<% if (isEnumImport(definition)) { -%>
+  import {
+    <% enumImports(definition).map((imp) => {-%>
+    <%= imp %>,
+    <%_ }) %>
+  } from '../../../cb.model';
+<%_ } %>
+describe('<%= classify(name)%> <%= classify(type) %> Map', () => {
+  let <%= camelize(name)%>Data;
+  let <%= camelize(name)%>: <%= classify(name)%>;
+  let mapper: <%= classify(name)%><%= classify(type) %>Map;
+  beforeAll(() => {
+    <%= camelize(name)%>Data = <%- flureeDataFixture(index, definition.name, fixtureDepth) %>
+    mapper = new <%= classify(name)%><%= classify(type) %>Map();
+  });
+
+  it('should create <%= classify(name) %> entity from Fluree data', () => {
+    <%= camelize(name)%> = mapper.toEntity(<%= camelize(name)%>Data);
+    expect(<%= camelize(name)%>.id).toBe('<%= fx.IDENTIFIER %>');
+    expect(<%= camelize(name)%>.subjectId).toBe('<%= fx.SUBJECT_ID %>');
+    expect(<%= camelize(name)%>.createdAt instanceof Date).toBeTruthy();
+    expect(<%= camelize(name)%>.updatedAt instanceof Date).toBeTruthy();
+    <%_ definition.props.map((p) => { -%>
+      <% if (createExpect(p)) { -%>
+        expect(<%= camelize(name)%>.<%= p.name %>).<%= assert(p) %>(<%- entityExample(p) %>);
+      <% } -%>
+    <%_ }) -%>
+  });
+
+  it('should serialize <%= classify(name) %> to Fluree data', () => {
+    const mappedData = mapper.fromEntity(<%= camelize(name)%>);
+    expect(mappedData).toStrictEqual(<%= camelize(name)%>Data);
+  });
+});
