03e3031c17db46635d5c0b2f4211857baf3f50fb
Dmitri Safine <dmitri.safine@adamantas.io>
2022-05-22 19:18:30 -0700

    LOG-154: DTO fixture generator

---
+++ libs/sdk/src/generators/utils/fixtures/dto/dto.fixture-generator.constants.ts
@@ -0,0 +1,13 @@
+export const predicates = {
+  CREATED_AT: 'createdAt',
+  UPDATED_AT: 'updatedAt',
+  ID: 'id',
+  SUBJECT_ID: 'subjectId',
+};
+
+export const fixtures = {
+  ID: '34b9142664b55f552ea66c8189ab481382d14385c126fee99a997b69e6b3824a',
+  CREATED_AT: '2022-05-22T20:53:18Z',
+  UPDATED_AT: '2022-05-22T20:53:18Z',
+  SUBJECT_ID: 369435906932826,
+};
---
+++ libs/sdk/src/generators/utils/fixtures/dto/dto.fixture-generator.ts
@@ -0,0 +1,64 @@
+import { Definition, DefinitionType, Property } from '@logosphere/converters';
+import {
+  fixtures as f,
+  predicates as p,
+} from './dto.fixture-generator.constants';
+import { propExample } from '@logosphere/converters';
+
+function generateDtoData(
+  defs: Definition[],
+  currentDef: Definition,
+  random = false,
+  maxDepth = 1,
+  currentDepth = 1
+): any {
+  const data = {};
+  data[p.SUBJECT_ID] = f.SUBJECT_ID;
+  data[p.ID] = f.ID;
+  data[p.CREATED_AT] = f.CREATED_AT;
+  data[p.UPDATED_AT] = f.CREATED_AT;
+
+  currentDef.props.map((prop: Property) => {
+    if (
+      prop.defType === DefinitionType.Entity ||
+      prop.defType === DefinitionType.EntityArray
+    ) {
+      if (currentDepth <= maxDepth) {
+        const refDef = defs.find((def: Definition) => def.name === prop.type);
+        const value = generateDtoData(
+          defs,
+          refDef,
+          random,
+          maxDepth,
+          currentDepth + 1
+        );
+        data[prop.name] =
+          prop.defType === DefinitionType.Entity ? value : [value];
+      } else {
+        data[prop.name] = null;
+      }
+    } else {
+      data[prop.name] = propExample(prop, random);
+    }
+  });
+
+  return data;
+}
+
+/**
+ * Generates DTO data fixture from canonical schema
+ * @param defs Canonical schema definitions
+ * @param rootDefName Name of the root definition
+ * @param random true, if taking random example from `examples` property.
+ * @param maxDepth max level of depth for nested data
+ * @returns DTO data fixture
+ */
+export function dtoData(
+  defs: Definition[],
+  rootDefName: string,
+  random = false,
+  maxDepth = 1
+) {
+  const rootDef = defs.find((d: Definition) => d.name === rootDefName);
+  return generateDtoData(defs, rootDef, random, maxDepth, 1);
+}
