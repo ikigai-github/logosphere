4d30cc2a961a52211dc23e475e55d1c9b27129a0
Dmitri Safine <dmitri.safine@adamantas.io>
2022-05-26 12:08:24 -0700

    LOG-164: GQL input types

--- libs/converters/src/gql/gql.generator.ts
+++ libs/converters/src/gql/gql.generator.ts
@@ -1,5 +1,5 @@
 import { pascalCase } from 'pascal-case';
-import { Definition, Property } from '../canonical';
+import { Definition, DefinitionType, Property } from '../canonical';
 import { Generator } from '../abstract/generator.abstract';
 import { GqlPropGenerator } from './gql.prop-generator';
 
@@ -17,12 +17,33 @@ export class GqlGenerator extends Generator {
   }
   protected generateEntity(def: Definition): string {
     const propGenerator = new GqlPropGenerator();
+
+    // regular GQL type
     let schemaString = `type ${pascalCase(def.name)} {\n`;
     def.props.forEach((prop: Property) => {
       if (prop.isEnabled) {
         schemaString += propGenerator.generate(prop);
       }
     });
+
+    schemaString += '}\n\n';
+
+    // input type (used in mutations and supposed to have only
+    // scalar fields or enums)
+    schemaString += `input ${pascalCase(def.name)}Input {\n`;
+    def.props
+      .filter(
+        (prop: Property) =>
+          prop.defType !== DefinitionType.Entity &&
+          prop.defType !== DefinitionType.EntityArray &&
+          prop.defType !== DefinitionType.ExternalEntity &&
+          prop.defType !== DefinitionType.ExternalEntityArray
+      )
+      .forEach((prop: Property) => {
+        if (prop.isEnabled) {
+          schemaString += propGenerator.generate(prop);
+        }
+      });
 
     schemaString += '}\n\n';
 
