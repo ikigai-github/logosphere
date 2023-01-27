532ac3cba16f762b6b2aa92deeb955a8ff849170
Dmitri Safine <dmitri.safine@adamantas.io>
2023-01-26 19:30:10 -0800

    fix(sdk): adjust dto generator to module lib with postfix

--- libs/sdk/src/generators/dto/dto.spec.ts
+++ libs/sdk/src/generators/dto/dto.spec.ts
@@ -1,22 +1,44 @@
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
-import { Tree } from '@nrwl/devkit';
+import { Tree, names, readProjectConfiguration } from '@nrwl/devkit';
 import generator from './dto';
-import { moduleGenerator }  from '../module';
+import { DEFAULT_LIB_CODEGEN_PREFIX } from '../../common';
+import { moduleGenerator } from '../module';
+import { definitions } from '../../test/fixtures/code-first/schemas/monolith/canonical/art-marketplace.canonical.schema.json';
 import { DtoGeneratorSchema } from './schema';
 
 describe('dto generator', () => {
   let tree: Tree;
-  const options: DtoGeneratorSchema = { name: 'wallet', module: 'test-app' };
+  const options: DtoGeneratorSchema = {
+    name: 'wallet',
+    module: 'testLib',
+    definition: definitions[0],
+  };
+  const libName = `${
+    names(options.module).fileName
+  }-${DEFAULT_LIB_CODEGEN_PREFIX}`;
 
-  beforeEach( async () => {
+  beforeEach(async () => {
     tree = createTreeWithEmptyWorkspace();
-    await moduleGenerator(tree, { 
-      name: 'test-app',
-      compiler: 'tsc'
-    })
+    await moduleGenerator(tree, {
+      name: options.module,
+      compiler: 'tsc',
+    });
   });
 
   it('should run successfully', async () => {
     await generator(tree, options);
-  })
+    const config = readProjectConfiguration(tree, libName);
+    expect(config).toBeDefined();
+    expect(config.root).toBe(libName);
+
+    //console.log(tree.listChanges().map(fc => console.log(fc.path)));
+  });
+
+  it('should generate files', async () => {
+    await generator(tree, options);
+    expect(tree.exists(`${libName}/src/dto/index.ts`)).toBeTruthy();
+    expect(tree.exists(`${libName}/src/dto/listing.dto.ts`)).toBeTruthy();
+    expect(tree.exists(`${libName}/src/dto/keys.dto.ts`)).toBeTruthy();
+    expect(tree.exists(`${libName}/src/dto/user-auth.dto.ts`)).toBeTruthy();
+  });
 });
