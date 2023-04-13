6a57adc8c6411217b5184d2c87ddf6d12c6c424c
Mike Morency <mikemorency93@gmail.com>
2023-04-12 23:43:43 -0400

    feat: bugfixes after testing the deploy

--- libs/sdk/src/generators/helm/files/deployment-scripts/deploy_helm_local.sh
+++ libs/sdk/src/generators/helm/files/deployment-scripts/deploy_helm_local.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-<% applications.forEach(function(application){ %>
-docker build -t logosphere/<%= application.name %>:latest ./<%= application.path %>
-<% }); %>
+set -e
 
-helm uninstall logosphere
-helm dependency build .
-helm install --create-namespace -n logosphere logosphere .
+echo node_modules >> .dockerignore
+<% applications.forEach(function(application){ %>
+docker build -t logosphere/<%= application.name %>:latest -f ./<%= application.path %>/Dockerfile .
+<% }); %>
+rm -f .dockerignore
+
+if helm list -n logo | grep -q logosphere; then
+    helm uninstall -n logo logosphere
+fi
+if helm repo list | grep -q ikigai-blockfrost; then
+    helm repo rm ikigai-blockfrost
+fi
+helm repo add ikigai-blockfrost http://ikigai-shared-helm-charts-public.s3-website-us-east-1.amazonaws.com/blockfrost
+helm dependency build ./helm
+helm install --create-namespace -n logo logosphere ./helm
